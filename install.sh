b2e8c6ed693c0586838e5c4149f18727f60e4441
programming101dev <113087924+programming101dev@users.noreply.github.com>
2025-01-31 11:19:53 -0800

    assumes that user has permission to write to the directories

--- install.sh
+++ install.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 
-if sudo cmake --install build; then
+skip_cache_update=false
+
+# Parse command-line options
+while getopts ":s" opt; do
+  case $opt in
+    s)
+      skip_cache_update=true
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+if cmake --install build; then
     echo "Installation successful."
 
     # Retrieve the owner of the 'build' directory
@@ -11,15 +26,17 @@ if sudo cmake --install build; then
     fi
 
     # Change the ownership of install_manifest.txt to match 'build' directory owner
-    sudo chown "$build_owner" build/install_manifest.txt
+    chown "$build_owner" build/install_manifest.txt
 
-    # Check if the command 'ldconfig' exists on the system
-    if command -v ldconfig >/dev/null; then
-        # 'ldconfig' exists, run it with sudo
-        sudo ldconfig
-    elif command -v update_dyld_shared_cache >/dev/null; then
-        # 'ldconfig' doesn't exist, but 'update_dyld_shared_cache' does, run it with sudo
-        sudo update_dyld_shared_cache -force
+    if [ "$skip_cache_update" = false ]; then
+        # Check if the command 'ldconfig' exists on the system
+        if command -v ldconfig >/dev/null; then
+            # 'ldconfig' exists, run it with sudo
+            sudo ldconfig
+        elif command -v update_dyld_shared_cache >/dev/null; then
+            # 'ldconfig' doesn't exist, but 'update_dyld_shared_cache' does, run it with sudo
+            sudo update_dyld_shared_cache -force
+        fi
     fi
 else
     echo "Installation failed or not supported."
